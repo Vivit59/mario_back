d37e9608d192677af258b1bcf74ce6fa91d16cef
Sylvie Debue <sdebue59@gmail.com>
2024-04-19 16:05:58 +0200

    signup with bcrypt but without restriction

--- src/main/java/fr/idformation/mario/security/Dto/UserDto.java
+++ src/main/java/fr/idformation/mario/security/Dto/UserDto.java
@@ -1,5 +1,8 @@
 package fr.idformation.mario.security.Dto;
 
+import java.util.Set;
+
+import fr.idformation.mario.security.models.Role;
 import fr.idformation.mario.security.models.User;
 
 /**
@@ -16,25 +19,33 @@ public class UserDto {
 	/** the lastname. */
 	private String lastname;
 
-	/** the role of the user. */
-	private String role;
+	/** the address. */
+	private String address;
+
+	/** the password. */
+	private String password;
+
+	/** the list of roles of the user. */
+	private Set<Role> roles;
 
 	/** Default constructor. */
 	public UserDto() {
 		super();
 	}
 
+
 	/**
-	 * Create a new UserDto from a User.
-	 *
+	 * create a user with all fields.
 	 * @param pUser the user to use
 	 */
 	public UserDto(final User pUser) {
+		super();
 		this.username = pUser.getUsername();
 		this.firstname = pUser.getFirstname();
 		this.lastname = pUser.getLastname();
-		this.role = pUser.getRoles().iterator().next().getName().name();
-
+		this.address = pUser.getAddress();
+		this.password = pUser.getPassword();
+		this.roles = pUser.getRoles();
 	}
 
 	/**
@@ -52,10 +63,38 @@ public class UserDto {
 	}
 
 	/**
-	 * @return the role
+	 * @return the address
 	 */
-	public String getRole() {
-		return role;
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param pAddress the address to set
+	 */
+	public void setAddress(final String pAddress) {
+		this.address = pAddress;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param pPassword the password to set
+	 */
+	public void setPassword(final String pPassword) {
+		this.password = pPassword;
+	}
+
+	/**
+	 * @return the roles
+	 */
+	public Set<Role> getRoles() {
+		return roles;
 	}
 
 	/**
@@ -80,10 +119,10 @@ public class UserDto {
 	}
 
 	/**
-	 * @param pRole the role to set
+	 * @param pRoles the roles to set
 	 */
-	public void setRole(final String pRole) {
-		this.role = pRole;
+	public void setRoles(final Set<Role> pRoles) {
+		this.roles = pRoles;
 	}
 
 	/**
